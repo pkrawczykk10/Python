4b9772f43d8135acf6a9fa55774553d796d3fd3f
pkrawczykk10 <polakra10@gmail.com>
2025-12-29 03:29:21 +0100

    Update najmniejszy.py

--- najmniejszy.py
+++ najmniejszy.py
@@ -1,12 +1,20 @@
 def najmniejszy():
-    liczby=[123,43,73,-17,-98,657,832,4,0,10]
-    najmniejsza=liczby[0]
-
+    liczby = []
+    
+    n = int(input("Ile liczb chcesz podać? "))
+    
+    for i in range(n):
+        x = int(input("Podaj liczbę: "))
+        liczby.append(x)
+    
+    najmniejsza = liczby[0]
+    
     for liczba in liczby:
-        if liczba<najmniejsza:
-            najmniejsza=liczba
-
-    print("Najmniejsza wartosc w liscie to:", najmniejsza)
-
+        if liczba < najmniejsza:
+            najmniejsza = liczba
+    
+    print("Najmniejsza liczba to:", najmniejsza)
+    input("Nacisnij ENTER aby wrocic do menu")
+    
 if __name__=='__main__':
     najmniejszy()
