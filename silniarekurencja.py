8188d1388714398964b75ef4032e16fe69a0ec3e
pkrawczykk10 <polakra10@gmail.com>
2025-12-29 03:27:35 +0100

    Update silniarekurencja.py

--- silniarekurencja.py
+++ silniarekurencja.py
@@ -1,11 +1,12 @@
+def silnia(n):
+    if n == 0:
+        return 1
+    return n * silnia(n - 1)
+
 def silniarekurencja():
-    def silnia(n):
-        if n == 0:
-            return 1
-        return n * silnia(n - 1)
+    n = int(input("Podaj n: "))
+    print("Silnia:", silnia(n))
+    input("Nacisnij ENTER aby wrocic do menu")
 
-    n = int(input("Podaj liczbę: "))
-    print(silnia(n))
-
-if __name__=='__main__':
+if __name__ == "__main__":
     silniarekurencja()
