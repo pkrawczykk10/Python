8131523f28b9f29ee913a591cb7077c4b3cd755b
pkrawczykk10 <polakra10@gmail.com>
2025-12-29 02:31:50 +0100

    Update ciagiteracja.py

--- ciagiteracja.py
+++ ciagiteracja.py
@@ -1,12 +1,12 @@
-def ciagiteracja():
-    n = int(input("Podaj ile elementów ciągu Fibonacciego: "))
+def silniaiteracja():
+    liczba=int(input("Podaj liczbe calkowita nieujemna:"))
+    if liczba<=0:
+        print("Silnia nie jest zdefiniowana dla liczb ujemnych.")
+    else:
+        silnia = 1
+        for i in range(1, liczba + 1):
+            silnia = silnia * i
+        print(f"{liczba}! = {silnia}")
 
-    a = 0
-    b = 1
-
-    for i in range(n):
-        print(a)
-        a, b = b, a + b
-        
-if __name__ == "__main__":
-    ciagiteracja()
+if __name__=='__main__':
+    silniaiteracja()
